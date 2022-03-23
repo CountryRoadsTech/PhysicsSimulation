bd45be12ad2887958170db669efa859e0d9511e9
Addison Martin <addisonejmartin@gmail.com>
2022-03-23 15:14:04 -0400

    Update scaffolded views

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
+# Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
+  # Create RESTful routes for scaffolds:
   resources :physics_bodies
   resources :universes
   resources :simulations
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "simulations#index"
 end
