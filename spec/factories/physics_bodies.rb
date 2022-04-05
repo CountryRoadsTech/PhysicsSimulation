4cf1a915c858a9d83d7f06e134b3eb986fccc7d7
Addison Martin <addisonejmartin@gmail.com>
2022-04-05 10:03:21 -0400

    Add application-wide configuration options for numerical precision and scale

--- spec/factories/physics_bodies.rb
+++ spec/factories/physics_bodies.rb
@@ -3,31 +3,31 @@
 # Table name: physics_bodies
 #
 #  id                 :uuid             not null, primary key
-#  accelerations_x    :decimal(, )      default([]), is an Array
-#  accelerations_y    :decimal(, )      default([]), is an Array
-#  accelerations_z    :decimal(, )      default([]), is an Array
-#  forces_x           :decimal(, )      default([]), is an Array
-#  forces_y           :decimal(, )      default([]), is an Array
-#  forces_z           :decimal(, )      default([]), is an Array
-#  initial_mass       :decimal(, )      not null
-#  initial_position_x :decimal(, )      not null
-#  initial_position_y :decimal(, )      not null
-#  initial_position_z :decimal(, )      not null
-#  initial_velocity_x :decimal(, )      not null
-#  initial_velocity_y :decimal(, )      not null
-#  initial_velocity_z :decimal(, )      not null
-#  masses             :decimal(, )      default([]), is an Array
+#  accelerations_x    :decimal(8, 5)    default([]), is an Array
+#  accelerations_y    :decimal(8, 5)    default([]), is an Array
+#  accelerations_z    :decimal(8, 5)    default([]), is an Array
+#  forces_x           :decimal(8, 5)    default([]), is an Array
+#  forces_y           :decimal(8, 5)    default([]), is an Array
+#  forces_z           :decimal(8, 5)    default([]), is an Array
+#  initial_mass       :decimal(8, 5)    not null
+#  initial_position_x :decimal(8, 5)    not null
+#  initial_position_y :decimal(8, 5)    not null
+#  initial_position_z :decimal(8, 5)    not null
+#  initial_velocity_x :decimal(8, 5)    not null
+#  initial_velocity_y :decimal(8, 5)    not null
+#  initial_velocity_z :decimal(8, 5)    not null
+#  masses             :decimal(8, 5)    default([]), is an Array
 #  name               :text             not null
-#  positions_x        :decimal(, )      default([]), is an Array
-#  positions_y        :decimal(, )      default([]), is an Array
-#  positions_z        :decimal(, )      default([]), is an Array
+#  positions_x        :decimal(8, 5)    default([]), is an Array
+#  positions_y        :decimal(8, 5)    default([]), is an Array
+#  positions_z        :decimal(8, 5)    default([]), is an Array
 #  slug               :text
-#  thrusts_x          :decimal(, )      default([]), is an Array
-#  thrusts_y          :decimal(, )      default([]), is an Array
-#  thrusts_z          :decimal(, )      default([]), is an Array
-#  velocities_x       :decimal(, )      default([]), is an Array
-#  velocities_y       :decimal(, )      default([]), is an Array
-#  velocities_z       :decimal(, )      default([]), is an Array
+#  thrusts_x          :decimal(8, 5)    default([]), is an Array
+#  thrusts_y          :decimal(8, 5)    default([]), is an Array
+#  thrusts_z          :decimal(8, 5)    default([]), is an Array
+#  velocities_x       :decimal(8, 5)    default([]), is an Array
+#  velocities_y       :decimal(8, 5)    default([]), is an Array
+#  velocities_z       :decimal(8, 5)    default([]), is an Array
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
 #  simulation_id      :uuid             not null
