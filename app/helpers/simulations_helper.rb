c0a9adb5bdc4bad4304d951f72424cfac806ff14
Addison Martin <addisonejmartin@gmail.com>
2022-04-05 17:43:26 -0400

    Add simulation solover helper methods

--- app/helpers/simulations_helper.rb
+++ app/helpers/simulations_helper.rb
@@ -1,2 +1,24 @@
 module SimulationsHelper
+  def solve(simulation, method='euler')
+    # Remove dashes, hypens, etc., whitespace, and changes to all one word, lower case.
+    method_stripped = method.titleize.gsub(/\s+/, '').lowercase
+
+    if method_stripped == 'euler'
+      first_order_runge_kutta_solution(simulation)
+    elsif method_stripped == 'rungekutta4'
+      fourth_order_runge_kutta_solution(simulation)
+    else
+      raise RuntimeError.new("Unknown solution method: #{method}! Try 'euler' or 'rungekutta4'.")
+    end
+  end
+
+  private
+
+  def first_order_runge_kutta_solution(simulation)
+
+  end
+
+  def fourth_order_runge_kutta_solution(simulation)
+    # TODO
+  end
 end
