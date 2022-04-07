13abaef96fb7981776a24a6f7d1ceab48db09add
Addison Martin <addisonejmartin@gmail.com>
2022-04-07 10:44:06 -0400

    Setup scaffold for calling Python solver from Rails app

--- app/helpers/simulations_helper.rb
+++ app/helpers/simulations_helper.rb
@@ -1,24 +1,41 @@
 module SimulationsHelper
-  def solve(simulation, method='euler')
-    # Remove dashes, hypens, etc., whitespace, and changes to all one word, lower case.
-    method_stripped = method.titleize.gsub(/\s+/, '').lowercase
+  PYTHON_PATH = File.join('/', 'usr', 'bin', 'python3')
+  PYTHON_WORKING_DIR = File.join(Rails.root, 'lib', 'python')
+  PYTHON_SCRIPT = File.join(PYTHON_WORKING_DIR, 'ode_solver.py')
 
-    if method_stripped == 'euler'
-      first_order_runge_kutta_solution(simulation)
-    elsif method_stripped == 'rungekutta4'
-      fourth_order_runge_kutta_solution(simulation)
-    else
-      raise RuntimeError.new("Unknown solution method: #{method}! Try 'euler' or 'rungekutta4'.")
+  def solve(simulation)
+
+
+    # TODO: Calculate and store computation time (start of this method to end, then save!, handle errors)
+
+
+    # Write simulation parameters to file, and call Python code to run ODE solver using Scipy.
+    input_path = File.join(PYTHON_WORKING_DIR, "simulation_#{simulation.name.parameterize}_parameters.input")
+
+    # Create the output path that will be used to read the results of the Python script.
+    output_path = File.join(PYTHON_WORKING_DIR, "simulation_#{simulation.name.parameterize}.output")
+
+    # Overwrite the existing input file if it exists.
+    File.open(input_path, 'w') do |input_file|
+      input_file.write('Hello Test!')
+      # Central body (Earth, Sun, all solar systems Mu plus radius)
+      # Each physics body (start with 1)
+      # Simulation name
     end
-  end
 
-  private
+    # Call the Python script to read the input file, run the ODE solver, and output the solution to a file.
+    output = `#{PYTHON_PATH} #{PYTHON_SCRIPT} #{input_path} #{output_path}`
 
-  def first_order_runge_kutta_solution(simulation)
+    puts "Command status:"
+    puts $CHILD_STATUS
+    puts output
 
-  end
-
-  def fourth_order_runge_kutta_solution(simulation)
-    # TODO
+    # Read the results of the Python script and save.
+    File.open(output_path, 'r') do |output_file|
+      puts "Output File:"
+      output_file.each do |output_line|
+        puts output_line
+      end
+    end
   end
 end
