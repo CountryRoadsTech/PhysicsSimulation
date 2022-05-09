a155b96dc0c70210d81e21c8dc80e780308dbd64
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2022-05-09 11:58:14 -0400

    Fix broken test

--- spec/views/simulations/show.html.tailwindcss_spec.rb
+++ spec/views/simulations/show.html.tailwindcss_spec.rb
@@ -1,17 +1,3 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-
-RSpec.describe 'simulations/show', type: :view do
-  before do
-    @simulation = assign(:simulation, create(:simulation))
-  end
-
-  it 'renders attributes in <p>' do
-    render
-    expect(rendered).to include(@simulation.name)
-    expect(rendered).to include(@simulation.description.to_s)
-    expect(rendered).to include(@simulation.computation_time.to_s)
-    expect(rendered).to include(@simulation.computed_at.to_s)
-  end
-end
