3817ec26ea9d45aea2f6225842975495bc31e874
Addison Martin <15254821+addisonmartin@users.noreply.github.com>
2022-05-09 10:11:24 -0400

    Run simulation's Javascript after page loads

--- app/javascript/source/simulation_renderer.js
+++ app/javascript/source/simulation_renderer.js
@@ -2,79 +2,81 @@ import * as THREE from 'three'
 import { OrbitControls } from 'OrbitControls'
 import { EXRLoader } from 'EXRLoader'
 
-// Get the HTML Canvas used as the target for the renderer
-const canvas = document.querySelector('#simulation_canvas')
-const renderer = new THREE.WebGLRenderer({canvas, alpha: true})
+document.addEventListener("turbo:load", function() {
+  // Get the HTML Canvas used as the target for the renderer
+  const canvas = document.querySelector('#simulation_canvas')
+  const renderer = new THREE.WebGLRenderer({canvas, alpha: true})
 
-// Add a perspective camera
-const fov = 75
-const aspect_ratio = canvas.width / canvas.height
-const near_render_distance = 1
-const far_render_distance = 10000
-const camera = new THREE.PerspectiveCamera(fov, aspect_ratio, near_render_distance, far_render_distance)
-camera.position.z = 2
+  // Add a perspective camera
+  const fov = 75
+  const aspect_ratio = canvas.width / canvas.height
+  const near_render_distance = 1
+  const far_render_distance = 10000
+  const camera = new THREE.PerspectiveCamera(fov, aspect_ratio, near_render_distance, far_render_distance)
+  camera.position.z = 2
 
-// Add orbit style controls
-const controls = new OrbitControls(camera, canvas)
-controls.target.set(0, 0, 0)
-controls.update()
+  // Add orbit style controls
+  const controls = new OrbitControls(camera, canvas)
+  controls.target.set(0, 0, 0)
+  controls.update()
 
-// Create the main scene
-const scene = new THREE.Scene()
+  // Create the main scene
+  const scene = new THREE.Scene()
 
-// Set the scene's background
-var currentSceneBackground = 'constellation_figures' // Default
-const textureLoader = new THREE.TextureLoader()
-const backgroundTexture = textureLoader.load('/resources/images/' + currentSceneBackground + '.png', () => {
-      const renderTarget = new THREE.WebGLCubeRenderTarget(backgroundTexture.image.height)
-      renderTarget.fromEquirectangularTexture(renderer, backgroundTexture)
-      scene.background = renderTarget.texture
-    })
+  // Set the scene's background
+  var currentSceneBackground = 'constellation_figures' // Default
+  const textureLoader = new THREE.TextureLoader()
+  const backgroundTexture = textureLoader.load('/resources/images/' + currentSceneBackground + '.png', () => {
+        const renderTarget = new THREE.WebGLCubeRenderTarget(backgroundTexture.image.height)
+        renderTarget.fromEquirectangularTexture(renderer, backgroundTexture)
+        scene.background = renderTarget.texture
+      })
 
-requestAnimationFrame(render)
-
-function render(time) {
-  time *= 0.001 // Convert time to seconds.
-
-  // Resize the camera if the canvas's size was changed
-  if (resizeRendererToDisplaySize(renderer)) {
-    const canvas = renderer.domElement
-    camera.aspect = canvas.clientWidth / canvas.clientHeight
-    camera.updateProjectionMatrix()
-  }
-
-  // Update the background image depending on the user's selection
-  checkAndChangeBackgroundImage(textureLoader, scene)
-
-  renderer.render(scene, camera)
   requestAnimationFrame(render)
-}
 
-function resizeRendererToDisplaySize(renderer) {
-  const canvas = renderer.domElement
-  const pixelRatio = window.devicePixelRatio
-  const width = canvas.clientWidth * pixelRatio | 1
-  const height = canvas.clientHeight * pixelRatio | 1
+  function render(time) {
+    time *= 0.001 // Convert time to seconds.
 
-  const needResize = canvas.width !== width || canvas.height !== height
-  if (needResize) {
-    renderer.setSize(width, height, false)
+    // Resize the camera if the canvas's size was changed
+    if (resizeRendererToDisplaySize(renderer)) {
+      const canvas = renderer.domElement
+      camera.aspect = canvas.clientWidth / canvas.clientHeight
+      camera.updateProjectionMatrix()
+    }
+
+    // Update the background image depending on the user's selection
+    checkAndChangeBackgroundImage(textureLoader, scene)
+
+    renderer.render(scene, camera)
+    requestAnimationFrame(render)
   }
 
-  return needResize
-}
+  function resizeRendererToDisplaySize(renderer) {
+    const canvas = renderer.domElement
+    const pixelRatio = window.devicePixelRatio
+    const width = canvas.clientWidth * pixelRatio | 1
+    const height = canvas.clientHeight * pixelRatio | 1
 
-function checkAndChangeBackgroundImage(textureLoader, scene) {
+    const needResize = canvas.width !== width || canvas.height !== height
+    if (needResize) {
+      renderer.setSize(width, height, false)
+    }
 
-  const userSelection = document.querySelector('input[name="background_image"]:checked').value;
-
-  if (userSelection !== currentSceneBackground) {
-    currentSceneBackground = userSelection
-
-    const backgroundTexture = textureLoader.load('/resources/images/' + userSelection + '.png', () => {
-          const renderTarget = new THREE.WebGLCubeRenderTarget(backgroundTexture.image.height)
-          renderTarget.fromEquirectangularTexture(renderer, backgroundTexture)
-          scene.background = renderTarget.texture
-        })
+    return needResize
   }
-}
+
+  function checkAndChangeBackgroundImage(textureLoader, scene) {
+
+    const userSelection = document.querySelector('input[name="background_image"]:checked').value;
+
+    if (userSelection !== currentSceneBackground) {
+      currentSceneBackground = userSelection
+
+      const backgroundTexture = textureLoader.load('/resources/images/' + userSelection + '.png', () => {
+            const renderTarget = new THREE.WebGLCubeRenderTarget(backgroundTexture.image.height)
+            renderTarget.fromEquirectangularTexture(renderer, backgroundTexture)
+            scene.background = renderTarget.texture
+          })
+    }
+  }
+})
